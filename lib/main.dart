33cf15c7ab6a91096128e23ae5bf1b3ddb708714
biniyam112 <biniyamdemissew112@gmail.com>
2021-06-26 13:31:29 +0300

    sights body complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:travel_app/constants.dart';
+import 'package:travel_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,32 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        scaffoldBackgroundColor: backgroundColor,
         primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
-      ),
     );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_app/constants.dart';
+
+import 'components/page_selection_tab.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,23 +12,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Text(
-            'Explore',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 20,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-            ),
-          ),
-        ],
-      ),
       body: Body(),
     );
   }
@@ -39,12 +28,83 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Explore',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 32,
+                      color: Colors.black.withOpacity(.8),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Center(
+                          child: Icon(
+                            FontAwesomeIcons.soundcloud,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             PageSelectionTab(),
             SizedBox(height: 30),
-            Text(
-              '10 Sights',
-              style: TextStyle(
-                fontSize: 18,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Text(
+                    '10 Sights',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SightItemBox(
+                    sightName: 'LightHouse \nExecursion',
+                    sightImage: 'assets/images/light_house.jpg',
+                    sightRating: 4.5,
+                    isbookmarked: true,
+                  ),
+                  SightItemBox(
+                    sightName: 'LightHouse \nExecursion',
+                    sightImage: 'assets/images/light_house_colored.jpg',
+                    sightRating: 2.4,
+                    isbookmarked: false,
+                  ),
+                  SightItemBox(
+                    sightName: 'Hawai island',
+                    sightImage: 'assets/images/nature.jpg',
+                    sightRating: 5,
+                    isbookmarked: true,
+                  ),
+                ],
               ),
             ),
           ],
@@ -54,72 +114,98 @@ class Body extends StatelessWidget {
   }
 }
 
-class PageSelectionTab extends StatefulWidget {
-  const PageSelectionTab({
+class SightItemBox extends StatelessWidget {
+  const SightItemBox({
     Key? key,
+    required this.sightName,
+    required this.sightImage,
+    required this.sightRating,
+    this.isbookmarked = false,
   }) : super(key: key);
+  final String sightName, sightImage;
+  final double sightRating;
+  final bool isbookmarked;
 
-  @override
-  _PageSelectionTabState createState() => _PageSelectionTabState();
-}
-
-class _PageSelectionTabState extends State<PageSelectionTab> {
-  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ...List.generate(
-          3,
-          (index) => TabItem(
-            onPress: () {
-              setState(() {
-                activeIndex = index;
-              });
-            },
-            title: 'Sight',
-            isActive: activeIndex == index,
+    return Container(
+      width: 300,
+      height: 240,
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            sightImage,
           ),
+          fit: BoxFit.cover,
         ),
-      ],
-    );
-  }
-}
-
-class TabItem extends StatelessWidget {
-  const TabItem({
-    Key? key,
-    required this.title,
-    required this.onPress,
-    this.isActive = false,
-  }) : super(key: key);
-  final String title;
-  final GestureTapCallback onPress;
-  final bool isActive;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPress,
-      child: Column(
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              color: isActive ? Colors.blue[400] : Colors.black,
-              fontWeight: FontWeight.w600,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.bookmark,
+                    color: isbookmarked ? primaryColor : primaryGrey,
+                  ),
+                ),
+              ),
             ),
-          ),
-          SizedBox(height: 6),
-          Container(
-            height: 5,
-            width: 5,
-            decoration: BoxDecoration(
-              color: isActive ? Colors.blue[400] : Colors.black,
-              shape: BoxShape.circle,
+            Spacer(),
+            Row(
+              children: [
+                RatingBar.builder(
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  glow: false,
+                  itemSize: 28,
+                  ignoreGestures: true,
+                  initialRating: sightRating,
+                  itemPadding: EdgeInsets.only(right: 5),
+                  itemBuilder: (context, _) {
+                    return Icon(
+                      Icons.star,
+                      color: Colors.amber[600],
+                    );
+                  },
+                  onRatingUpdate: (rating) {},
+                ),
+                SizedBox(width: 8),
+                Text(
+                  '$sightRating',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                sightName,
+                style: TextStyle(
+                  height: 1.1,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
